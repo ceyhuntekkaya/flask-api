18c66894e11aeeeda5fc9519120dfdcecb558aaf
ceyhuntekkaya <ceyhun.tekkaya@gmail.com>
2023-05-29 17:28:23 +0300

    control

---
+++ CONTRIBUTING.md
@@ -0,0 +1,1 @@
+# Genixo Bilişim ve Teknoloji
--- app.py
+++ app.py
@@ -7,16 +7,21 @@ from flask_migrate import Migrate
 from db import db
 from blocklist import BLOCKLIST
 
+from dotenv import load_dotenv
+
 import secrets
+import os
 
 from resources.user import blp as UserBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 
+# RENDER.COM    	"url": "https://flask-api-dlhg.onrender.com",
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -25,7 +30,7 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
@@ -35,7 +40,7 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] =  "jose" # secrets.SystemRandom().getrandbits(128) 
     jwt = JWTManager(app)
 
-
+    #op.execute("UPDATE tablo_adi SET kolon_adi='Default_value' ")
 
 
     # @jwt.additional_claims_loader
